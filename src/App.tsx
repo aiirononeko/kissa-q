724b6c2d381ae0f4770fe004ce9842ce354c30fe
aiirononeko <aiirononeko@gmail.com>
2022-10-14 20:35:03 +0900

    tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { ApolloClient, InMemoryCache, gql, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+})
 
 function App() {
+
+  useEffect(() => {
+    client.query({
+      query: gql`
+        query GetRates {
+          rates(currency: "USD") {
+            currency
+          }
+        }
+      `
+    }).then((result) => console.log(result));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Apollo Client</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
